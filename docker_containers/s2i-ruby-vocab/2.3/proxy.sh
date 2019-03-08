3e60cdb38e4e11cd8a5fb4963e718a9715ffda39
Tim Taylor <ttaylor@mitre.org>
2019-03-07 19:27:19 -0500

    Move the s2i-ruby-vocab builder image to its own repo
    See https://github.com/CDCgov/s2i-ruby-vocab-builder

--- docker_containers/s2i-ruby-vocab/2.3/proxy.sh
+++
@@ -1,4 +0,0 @@
-http_proxy="http://gatekeeper.mitre.org:80/"
-https_proxy="http://gatekeeper.mitre.org:80/"
-no_proxy="localhost,127.0.0.1,.mitre.org,.xip.io"
-export http_proxy https_proxy no_proxy
